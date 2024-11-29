56f1e7052ea190e9f14f5ffe124f56275124dd87
houji <1823523855@qq.com>
2024-11-30 00:53:50 +0800

    feat: build directory

--- cmd/apylee_chat_server/main.go
+++ cmd/apylee_chat_server/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"apylee_chat_server/internal/service"
+	v1 "apylee_chat_server/api/v1"
 	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
 	r := gin.Default()
-	r.POST("/login", service.Login)
-	r.POST("/register", service.Register)
+	r.POST("/login", v1.Login)
+	r.POST("/register", v1.Register)
 	if err := r.Run("127.0.0.1:8080"); err != nil {
 		log.Fatal("server running fault")
 		return
